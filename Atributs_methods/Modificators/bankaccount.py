b5c9d1756a3dd82c57d5eac5298a1680a26cd35c
Maiia Orlovsky <orlovsky-maya@mail.ru>
2025-02-14 09:17:34 +0300

    Add bankaccount.py to Modificators part

---
+++ Atributs_methods/Modificators/bankaccount.py
@@ -0,0 +1,34 @@
+from typing import Type
+
+
+class BankAccount:
+    def __init__(self, balance=0):
+         self._balance = balance
+
+    def get_balance(self):
+        return self._balance
+
+
+    def deposit(self, amount):
+        self._balance += amount
+        return self._balance
+
+
+    def withdraw(self, amount):
+        if amount > self._balance:
+            raise ValueError ('На счете недостаточно средств')
+        else:
+            self._balance -= amount
+
+    def transfer(self, account: Type['BankAccount'], amount):
+        self.withdraw(amount)
+        account.deposit(amount)
+
+
+account1 = BankAccount(100)
+account2 = BankAccount(200)
+
+try:
+    account1.transfer(account2, 150)
+except ValueError as e:
+    print(e)
