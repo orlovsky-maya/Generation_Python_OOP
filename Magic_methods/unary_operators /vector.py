dad14cc817dbe75e7b06c760db22f11863cd903a
Maiia Orlovsky <orlovsky-maya@mail.ru>
2025-07-21 09:45:26 +0300

    Add solutions for unary operators and functions

---
+++ Magic_methods/unary_operators /vector.py
@@ -0,0 +1,35 @@
+import  math
+
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f"Vector({self.x}, {self.y})"
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __pos__(self):
+        return Vector(self.x, self.y)
+
+    def __neg__(self):
+        return Vector(-self.x, -self.y)
+
+    def __abs__(self):
+        return math.sqrt(self.x ** 2 + self.y ** 2)
+
+vector = Vector(3, -4)
+
+print(+vector)
+print(-vector)
+print(abs(vector))
+
+
+vector = Vector(-8, -6)
+
+print(+vector)
+print(-vector)
+print(abs(vector))
+print(repr(vector))
